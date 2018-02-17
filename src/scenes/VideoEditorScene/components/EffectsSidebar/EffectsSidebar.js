51bed2815803163751309cf7d70e2736c57cf503
n0190 <0910.nanami@gmail.com>
2018-02-17 12:35:11 +0500

    [effects-sidebar] refactor the sidebar structure to be flat and more general

--- src/scenes/VideoEditorScene/components/EffectsSidebar/EffectsSidebar.js
+++ src/scenes/VideoEditorScene/components/EffectsSidebar/EffectsSidebar.js
@@ -1,65 +1,114 @@
 import React, { Component } from "react";
 
-import EffectsGroup from "./widgets/EffectsGroup/EffectsGroup";
+import CollapsiblePane from "./widgets/CollapsiblePane/CollapsiblePane";
 import "./EffectsSidebar.css";
 
 class EffectsSidebar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.effectGroups = [
-			{
-				name: "Group 1",
-				effects: [
-					{
-						name: "Video",
-						params: [
-							{
-								name: "File",
-								value: "video.mp4",
-							},
-							{
-								name: "Looped",
-								value: false,
-							},
-							{
-								name: "Frame",
-								value: "whatever",
-							},
-							{
-								name: "Alpha",
-								value: 1,
-							},
-						],
-					},
-					{
-						name: "SimpleSpectrum",
-						params: [
-							{
-								name: "barsCount",
-								value: 2,
-							}, {
-								name: "scale",
-								value: "value",
-							},
-						],
-					},
-				],
-			},
-			{
-				name: "Group Text",
-				effects: [
-					{
-						name: "Cool Text Effect",
-						params: [],
-					},
-				],
-			},
-		];
+		this.buildContent = this.buildContent.bind(this);
+
+		this.state = {
+			elements: [
+				{
+					id: 1,
+					type: "group",
+					name: "Group 1",
+					parent: null,
+				},
+				{
+					id: 2,
+					type: "group",
+					name: "Group Text",
+					parent: null,
+				},
+				{
+					id: 3,
+					type: "effect",
+					name: "Effect with no group",
+					parent: null,
+					params: [
+						{
+							name: "param1",
+							value: "value1",
+						}, {
+							name: "param2",
+							value: "value2",
+						},
+					],
+				},
+				{
+					id: 4,
+					type: "effect",
+					name: "Video",
+					parent: 1,
+					params: [
+						{
+							name: "File",
+							value: "video.mp4",
+						},
+						{
+							name: "Looped",
+							value: false,
+						},
+						{
+							name: "Frame",
+							value: "whatever",
+						},
+						{
+							name: "Alpha",
+							value: 1,
+						},
+					],
+				},
+				{
+					id: 5,
+					type: "effect",
+					name: "SimpleSpectrum",
+					parent: 1,
+					params: [
+						{
+							name: "barsCount",
+							value: 2,
+						}, {
+							name: "scale",
+							value: "value",
+						},
+					],
+				},
+				{
+					id: 6,
+					type: "effect",
+					name: "Cool Text Effect",
+					parent: 2,
+					params: [],
+				},
+			],
+		};
+	}
+
+	buildContent(parent) {
+		const elementsOnLevel = this.state.elements.filter(element => element.parent === parent).map((element) => {
+			const elementWithContent = Object.assign({}, element);
+			if (elementWithContent.type === "group") {
+				elementWithContent.content = this.buildContent(elementWithContent.id);
+			} else {
+				const params = elementWithContent.params.map(param => (<li key={param.name}>{param.name}: {param.value}</li>));
+				elementWithContent.content = (<ul>{params}</ul>);
+			}
+			return elementWithContent;
+		}).map((element) => {
+			const collapsiblePane = (<CollapsiblePane name={element.name} content={element.content} />);
+			return (
+				<li key={element.id}>{collapsiblePane}</li>
+			);
+		});
+		return (<ul>{elementsOnLevel}</ul>);
 	}
 
 	render() {
-		const items = this.effectGroups.map(group => (<li key={group.name} className="sidebar__group"><EffectsGroup group={group} /></li>));
+		const items = this.buildContent(null);
 		return (
 			<div className="sidebar">
 				<ul>
